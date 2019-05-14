4f31cac62a1098f4f86b4eef4091ee973ec68d44
Mélissa Loeser <melissa.loeser@gmail.com>
2019-05-14 11:07:25 +0200

    update meta desription

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <nav class="navbar navbar-expand-lg u-bg-color-default">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation" @click="toggleNavBar">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <nuxt-link class="nav-brand" :to="{name: 'index'}">NavBar <span class="sr-only"></span></nuxt-link>
+    <nuxt-link class="nav-brand" :to="{name: 'index'}">Rainbow Unicorn <span class="sr-only"></span></nuxt-link>
 
     <div :class="`collapse navbar-collapse ${navBar && 'show'}`" id="navbarTogglerDemo03">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
@@ -19,7 +19,7 @@
       </ul>
         <RbUForm @submit="searchAction" class="form-inline my-2 my-lg-0">
           <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" v-model="search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button class="btn" type="submit">Search</button>
         </RbUForm>
     </div>
   </nav>
