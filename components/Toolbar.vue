01c89480eb83a4379826440b16f27a7415dda7c1
Mélissa Loeser <melissa.loeser@gmail.com>
2019-05-15 12:28:27 +0200

    Merge branch 'master' of github.com:azaryia/nuxt-js
    # Conflicts:
    #	pages/admin/index.vue
    #	pages/blog/index.vue²

---
+++ components/Toolbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="toolbar">
+    <div class="toolbar__inner">
+      <div class="toolbar__main-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
